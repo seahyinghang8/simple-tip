20ff21b8fbca7cd42cd2d73f273d0e612030db4a
Seah Ying Hang <seahyinghang8@Seahs-MBP.hsd1.ca.comcast.net>
2020-10-02 22:14:35 -0700

    Empty project

---
+++ app/src/main/java/edu/stanford/seahyinghang8/simpletip/MainActivity.kt
@@ -0,0 +1,11 @@
+package edu.stanford.seahyinghang8.simpletip
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
