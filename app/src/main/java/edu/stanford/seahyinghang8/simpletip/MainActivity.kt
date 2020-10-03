81f9d214b2dea56d71a142c7b7b6ddfce34dac85
Seah Ying Hang <seahyinghang8@Seahs-MBP.hsd1.ca.comcast.net>
2020-10-03 00:48:34 -0700

    Base amount and Tip containers created with highlights

--- app/src/main/java/edu/stanford/seahyinghang8/simpletip/MainActivity.kt
+++ app/src/main/java/edu/stanford/seahyinghang8/simpletip/MainActivity.kt
@@ -2,10 +2,38 @@ package edu.stanford.seahyinghang8.simpletip
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+private const val TAG = "MainActivity"
+
+enum class ContainerSelected {
+    BASE, TIP
+}
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        baseContainer.setOnClickListener {
+            highlightContainer(ContainerSelected.BASE)
+        }
+
+        tipContainer.setOnClickListener {
+            highlightContainer(ContainerSelected.TIP)
+        }
+    }
+
+    private fun highlightContainer(selected: ContainerSelected) {
+        baseHighlight.visibility = View.GONE
+        tipHighlight.visibility = View.GONE
+
+        when (selected) {
+            ContainerSelected.BASE -> baseHighlight.visibility = View.VISIBLE
+            ContainerSelected.TIP -> tipHighlight.visibility = View.VISIBLE
+        }
     }
 }
